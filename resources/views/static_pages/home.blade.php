d05beb16584502b1582a5741be83f25b090eb91e
chenyang <1194884851@qq.com>
2021-09-27 11:14:38 +0800

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,19 @@
 
 @section('title')
 @section('content')
-    <h1>首页</h1>
+    <div class="jumbotron">
+      <h1> Hello Laravel</h1>
+
+      <p class="lead">
+        欢迎来到 <a href="#">Laravel 框架 开发应用</a>
+      </p>
+
+      <p class="lead">
+        一切,将从这里开始。
+      </p>
+
+      <p>
+        <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+      </p>
+    </div>
 @stop
