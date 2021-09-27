39690d08e908f0396ac9a4dddd0040453c206a88
chenyang <1194884851@qq.com>
2021-09-27 11:49:43 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" alt="">
+  <a href="https://laravel.com/" target="_blank">此刻的努力与汗水,总会成为以后迈向成功的资本与傲气!</a>
+
+  <div class="float-right">
+    <a href="about">关于我们</a>
+  </div>
+</footer>
