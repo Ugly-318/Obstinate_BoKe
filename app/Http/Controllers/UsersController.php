e80dc90247e5729fd066f44144543fed0a190088
chenyang <1194884851@qq.com>
2021-09-29 11:32:18 +0800

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,6 +17,10 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    /**
+     * @param Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
     public function store(Request $request)
     {
         $this->validate($request, [
@@ -24,6 +28,15 @@ class UsersController extends Controller
            'email' => 'required|unique:users|max:255',
            'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password)
+        ]);
+
+        session()->flash('success', '欢迎, 您将在这里开始一段新的旅程~');
+//        dd(session()->get('success'));
+        return redirect()->route('users.show', [$user]);
     }
 }
