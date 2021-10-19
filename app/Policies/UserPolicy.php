af99da0e01d6df53e15607eeaf602b919cacb839
chenyang <1194884851@qq.com>
2021-10-19 17:59:35 +0800

    修改线上编辑个人信息问题

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,10 +9,13 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    // 用户只能编辑自己的资料
     public function update(User $currentUser, User $user)
     {
         return $currentUser->id === $user->id;
     }
+
+    // 当前用户拥有管理员权限且删除用户不能是自己
     public function destroy(User $currentUser, User $user)
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
