c7ad99a4bc0d250241352586f38279de9676bdce
chenyang <1194884851@qq.com>
2021-10-19 17:08:43 +0800

    移除邮件发送的 form 选项

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -118,13 +118,13 @@ class UsersController extends Controller
     {
         $view = 'emails.confirm';
         $data = compact('user');
-        $from = '1194884851@qq.com';
-        $name = '任晨阳';
+//        $from = '1194884851@qq.com';
+//        $name = '任晨阳';
         $to = $user->email;
         $subject = "感谢注册 Obstinate App 应用! 请确认你的邮箱。";
 
-        Mail::send($view, $data, function ($message) use ($from, $name, $to, $subject ) {
-            $message->from($from, $name)->to($to)->subject($subject);
+        Mail::send($view, $data, function ($message) use ($to, $subject ) {
+            $message->to($to)->subject($subject);
         });
     }
 
