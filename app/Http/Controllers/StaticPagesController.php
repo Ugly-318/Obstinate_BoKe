252030f8078d0221417ee556e2fb02bff1da7ac2
chenyang <1194884851@qq.com>
2021-10-21 02:48:03 +0800

    删除微博

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -13,6 +13,7 @@ class StaticPagesController extends Controller
         if (Auth::check()) {
             $feed_items = Auth::user()->feed()->paginate(30);
         }
+//        dd($feed_items->toArray());
 
         return view('static_pages/home', compact('feed_items'));
     }
