3cc52cd6d8bfb1573243ea39658a30fd77c5bb2b
chenyang <1194884851@qq.com>
2021-10-24 13:54:20 +0800

    粉丝数据表

--- app/Models/User.php
+++ app/Models/User.php
@@ -71,4 +71,41 @@ class User extends Authenticatable
         return $this->statuses()
             ->orderBy('created_at', 'desc');
     }
+
+    // 获取粉丝
+    public function followers()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'user_id', 'follower_id');
+    }
+
+    // 获取关注
+    public function followings()
+    {
+        return $this->belongsToMany(User::class, 'followers', 'follower_id', 'user_id');
+    }
+
+    // 关注用户
+    public function follow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->sync($user_ids, false);
+    }
+
+    // 取消关注
+    public function unfollow($user_ids)
+    {
+        if (! is_array($user_ids)) {
+            $user_ids = compact('user_ids');
+        }
+        $this->followings()->detach($user_ids);
+    }
+
+    // 判断当前登录用户 A 是否已经关注 B
+    public function isFollowing($user_ids)
+    {
+        return $this->followings->contains($user_ids);
+    }
+
 }
