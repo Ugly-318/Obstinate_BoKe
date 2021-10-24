24e445df78cdaab144bbc3e55ca1d10b2ca2506c
chenyang <1194884851@qq.com>
2021-10-24 17:18:55 +0800

    关注按钮

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -20,6 +20,12 @@ class UserPolicy
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
     }
+
+    // 用户自己不能关注自己
+    public function follow(User $currentUser, User $user)
+    {
+        return $currentUser->id !== $user->id;
+    }
     /**
      * Create a new policy instance.
      *
